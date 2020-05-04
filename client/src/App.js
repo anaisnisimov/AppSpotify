5f2064d0f84be3405112bc1bc8afac43d8ef7c89
anaisnisimov <anais.nisimov@gmail.com>
2020-05-04 10:05:02 +0200

    Revert "connexion to apiSpotify with node and reception of spotify token in front"
    This reverts commit e1937d880e015783fe68eb1ef30dd8660185e46f.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import Axios from "axios";
-import Search from './Search';
 
 function App() {
   Axios({
@@ -11,12 +11,25 @@ function App() {
       "Content-Type": "application/json"
     }
   }).then(res => {
-    console.log(res.data.message, "le token est :", res.data.body);
+    console.log(res.data.message);
   });
 
   return (
     <div className="App">
-    <Search/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- server-connect/server.js
+++ server-connect/server.js
@@ -1,6 +1,3 @@
-const querystring = require('querystring');
-const Axios = require('axios');
-require('dotenv').config();
 const express = require("express"),
     app = express(),
     port = process.env.PORT || 5000,
@@ -9,31 +6,6 @@ const express = require("express"),
 app.use(cors());
 app.listen(port, () => console.log("Backend server live on " + port));
 
-
 app.get("/", (req, res) => {
-
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
-    }
-        Axios.post('https://accounts.spotify.com/api/token?grant_type=client_credentials', querystring.stringify({}), {
-          headers
-        })
-        .then(function(response) {
-            //console.log('token: ' + response.data.access_token)
-
-            const responseObject = {
-            statusCode: 200,
-            body: (response.data.access_token),
-            message: "we did it !"
-        };
-        res.send(responseObject);
-        //console.log(responseObject);
-        
-    })
-    .catch(function(err) {
-        console.error("Error: " + err);
-        
-    });
-    
+    res.send({ message: "We did it!" });
 });
