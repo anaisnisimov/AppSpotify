e1937d880e015783fe68eb1ef30dd8660185e46f
anaisnisimov <anais.nisimov@gmail.com>
2020-05-04 10:00:50 +0200

    connexion to apiSpotify with node and reception of spotify token in front

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Axios from "axios";
+import Search from './Search';
 
 function App() {
   Axios({
@@ -11,25 +11,12 @@ function App() {
       "Content-Type": "application/json"
     }
   }).then(res => {
-    console.log(res.data.message);
+    console.log(res.data.message, "le token est :", res.data.body);
   });
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Search/>
     </div>
   );
 }
